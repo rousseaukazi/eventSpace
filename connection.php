7083080b62ac4f6f4954696ef15b0ab37a72cf93
rousseau <kazi.rousseau@gmail.com>
2012-11-10 17:43:04 -0800

    changed ip address

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 
  
  // Connects to your Database 
- mysql_connect("localhost", "suman", "ninjas1158!") or die(mysql_error()); 
+ mysql_connect("174.120.60.130", "suman", "ninjas1158!") or die(mysql_error()); 
  mysql_select_db("suman_eventSpace") or die(mysql_error()); 
 
 $data = mysql_query("SELECT * FROM Users") 
