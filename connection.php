7916047210f05ab5baa6f4c6bf8b6396506d2741
sumanv <svenkat45@gmail.com>
2012-11-10 22:22:41 -0800

    First version of index.php using SQL queries

--- connection.php
+++ connection.php
@@ -5,7 +5,5 @@
  mysql_connect("174.120.60.130", "suman", "ninjas1158!") or die(mysql_error()); 
  mysql_select_db("suman_eventSpace") or die(mysql_error()); 
 
-$data = mysql_query("SELECT * FROM Users") 
- or die(mysql_error()); 
 
 ?>
--- index.php
+++ index.php
@@ -10,14 +10,9 @@
 		
 		// begin dashboard
 			$setTargetWidth = 500;
-		<?php 
-			$setTxtFile = "day_three_test.txt";
+		<?php 		
 			$setPageTitle = "Event Space Testing, Day 3"; 
-			$album_name_array = array(
-				"bond",
-				"sadface", 
-				"picture"
-			);
+			
 		?>	
 		// end dashboard
 
@@ -65,71 +60,19 @@
 
 
 	<?php
+		include "connection.php";
 
-		#counts the number of lines in the file
-		$file=$setTxtFile;
-		$linecount = 0;
-		$handle = fopen($file, "r");
-		while(!feof($handle)){
-  			$line = fgets($handle);
-  			$linecount++;
-		}
+		$data = mysql_query("SELECT * FROM Entries");
+	
+		while($info = mysql_fetch_array($data )) 
+		{
+			echo $info['Picture'];
+			echo '<div data-role="header" data-theme="d" data-content-theme="d"><h1> Album: '. $info['Album'] . '</h1></div>';
+			echo '<div data-role="content" align="center">';
+			echo '<div id="wrapper"><div id="content"><img src="' . $info['Picture'] . '"</div></div></div><br><br>';
+		} 
 
-		fclose($handle);
-
-		#looks for all lines beginning w "h" and w "1"
-		#and appends them to the end of the arrays 
-		#$picture_array and $number_array, 
-		$myFile = $setTxtFile;
-		$fh = fopen($myFile, 'r');
-		$picture_array = array();
-		$number_array = array();
-		$keyword_array = array();
-		for ($i = 1; $i <= $linecount; $i++) {
-			$dataLine = fgets($fh);
-			if ($dataLine[0] == "h") {
-				array_push($picture_array, $dataLine);
-			} else if ($dataLine[0] == '1'){
-				array_push($number_array, $dataLine);
-			} else {
-				array_push($keyword_array, strtolower(trim(preg_replace( '/[^[:print:]]/', '',$dataLine))));
-			}
-		}
-
-		#array that makes a dictionary where the keys
-		#== the values from $number_array and give 
-		#names associated w the numbers as values
-		$number_to_name = array(
-		    16508420492 => "Rousseau Kazi",
-		    18042294822 => "Channing Allen",
-		    18477226071 => "Suman Venkataswamy",
-		    14086246110 => "Ashley Malone",
-		    19518928892 => "Ashley Bennett",
-		  	16505213837 => "Alexa Krakaris",
-		  	18472716925 => "Jonathon Paul",
-		  	19519929201 => "Jelani Hayes",
-		  	14157866212 => "Sumaya Kazi",
-		  	12532283438 => "Joshua Evenson",
-		  	19413024516 => "Rob Balian",
-		  	17143920591 => "Evan Kawahara",
-		  	19145847487 => "Maddie Boyd",
-		  	13046155153 => "Addison Litton",
-		  	15514047775 => "Christopher Lo"
-		);
-
-		for ($l = 0; $l < count($album_name_array); $l++) {
-				echo '<div data-role="header" data-theme="d" data-content-theme="d"><h1> Album: '. $album_name_array[$l] . '</h1></div>';
-				echo '<div data-role="content" align="center">';
-				for ($j = count($picture_array); $j >= 0; $j--) {
-					if ($keyword_array[$j] == $album_name_array[$l]) {
-						echo '<div>' . $number_to_name[intval($number_array[$j])] . '</div><div id="wrapper"><div id="content"><img src="' . $picture_array[$j] . '"</div></div></div><br><br>';
-					} 
-				}
-				echo '</div>';
-			}
-
-			fclose($fh);
-		?>
+	?>
 
 
 </div><!-- /page -->
