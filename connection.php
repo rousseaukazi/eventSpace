ec61ff10313a133013692a17d11e858c36c48455
Suman Venkataswamy <svenkat45@gmail.com>
2012-11-11 22:14:46 -0800

    Update connection.php
    Directions on what the host server name should be

--- connection.php
+++ connection.php
@@ -2,6 +2,12 @@
 
  
  // Connects to your Database 
+
+//////////
+IMPORTANT!!! If connecting to Suman's hostGator server remotely, host is "174.130.60.130". 
+else, you are hosting this file on Suman's hostGator server so the host is simply "localhost"
+//////////
+
  mysql_connect("174.120.60.130", "suman", "ninjas1158!") or die(mysql_error()); 
  mysql_select_db("suman_eventSpace") or die(mysql_error()); 
 
