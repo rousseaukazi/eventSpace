001828788a3d71e65670895b75ea87bb8ea76a49
sumanv <svenkat45@gmail.com>
2012-11-11 22:10:56 -0800

    callbck.php now working again. could not figure out problem. It has something to do with the database table and not the code. Maybe something with permissions? Fixed this by dropping the table and creating a new one....

--- callback.php
+++ callback.php
@@ -1,6 +1,6 @@
 <?php
 
-include 'connection.php';
+include "connection.php";
 
 /////////////////////////////////////
 //Parsing & Extracting the XML object
@@ -13,51 +13,62 @@ $image = $simple_xml->images->image;
 $phone = $simple_xml->msisdn;
 $message = $simple_xml->message;
 $message = strtolower(trim($message));
+$messageArray = explode(" ", $message);
+
+// ///////
+// //Logic
+// ///////    
+
+// /////////////
+// //Album Logic
+// /////////////
+
+// if ($messageArray[0] == "album"){
+//     $url = 'https://api.mogreet.com/moms/transaction.send?client_id=1316&token=dbd7557a6a9d09ab13fda4b5337bc9c7&campaign_id=28420&to=' . intval($phone) . '&message=www.sumanvenkat.com/&format=json';
+//     $ch = curl_init($url); 
+//     $response = curl_exec($ch);
+//     curl_close($ch);
+// } 
+
+// // /////////////////
+// // //Megaphone Logic
+// // /////////////////
 
 
-///////
-//Logic
-///////    
+// elseif($messageArray[0] == "megaphone") {
+//     unset($messageArray[0]);
+    
+//     $messageEncoded = urlencode(implode(" ", $messageArray));
 
-if (count(explode(" ", $message)) > 1) {
+//     $data = mysql_query("SELECT Phone From TestUsers");
 
-/////////////////
-//Megaphone Logic
-/////////////////
+//     while($info = mysql_fetch_array($data)){
+//         $url = 'https://api.mogreet.com/moms/transaction.send?client_id=1316&token=dbd7557a6a9d09ab13fda4b5337bc9c7&campaign_id=28420&to=' . intval($info['Phone']) . '&message=' . $messageEncoded . '&format=json';
+//         $ch = curl_init($url); 
+//         $response = curl_exec($ch);
+//         curl_close($ch);   
+//     }
 
-    $messageArray = explode(" ", $message);
-    if($messageArray[0] == "megaphone") {
-        unset($messageArray[0]);
-        $messageEncoded = urlencode(implode(" ", $messageArray));
-        foreach ($number_to_name as $key => $value) {
-            $url = 'https://api.mogreet.com/moms/transaction.send?client_id=1316&token=dbd7557a6a9d09ab13fda4b5337bc9c7&campaign_id=28420&to=' . intval($key) . '&message=' . $messageEncoded . '&format=json';
-            $ch = curl_init($url); 
-            $response = curl_exec($ch);
-            curl_close($ch);            
-        } 
+// }
+   
 
-///////////////
-//Invite Logic
-///////////////
+// // ///////////////
+// // //Invite Logic
+// // ///////////////
 
-    } elseif ($messageArray[0] == "invite") {
-            $welcomeString = "Hey! Welcome to Event Space!";
-            $url = 'https://api.mogreet.com/moms/transaction.send?client_id=1316&token=dbd7557a6a9d09ab13fda4b5337bc9c7&campaign_id=28420&to=' . intval(trim($somecontent_rcs_invite[1])) . '&message=' . $welcomeString . '&format=json';
-            $ch = curl_init($url); 
-            $response = curl_exec($ch);
-            curl_close($ch);
-        }
+// elseif ($messageArray[0] == "invite") {
+//     $welcomeString = "Hey! Welcome to Event Space!";
+//     mysql_query("INSERT INTO TestUsers VALUES ('$messageArray[1]'");
+//     $url = 'https://api.mogreet.com/moms/transaction.send?client_id=1316&token=dbd7557a6a9d09ab13fda4b5337bc9c7&campaign_id=28420&to=' . intval(trim($messageArray[1]) . '&message=' . $welcomeString . '&format=json';
+//     $ch = curl_init($url); 
+//     $response = curl_exec($ch);
+//     curl_close($ch);
+// }
 
-/////////////
-//Album Logic
-/////////////
 
-    } elseif($message == "album"){
-        $url = 'https://api.mogreet.com/moms/transaction.send?client_id=1316&token=dbd7557a6a9d09ab13fda4b5337bc9c7&campaign_id=28420&to=' . intval($somecontent_rcs_number) . '&message=www.rousseaukazi.com/&format=json';
-        $ch = curl_init($url); 
-        $response = curl_exec($ch);
-        curl_close($ch);
-    } else { 
+
+
+// else { 
 
 
 
@@ -66,22 +77,30 @@ if (count(explode(" ", $message)) > 1) {
 ////////////////////////////
 
 
-mysql_query("INSERT INTO Entries VALUES ('$phone', NOW(),'$image', '$message')");
+    mysql_query("INSERT INTO Entries (Phone, Picture, Album) VALUES ('$phone', '$image', '$message')");
 
+    $thankYouMsg = "Awesome!_Check_out_your_picture_at_the_album_URL!!!";
 
+    $url = 'https://api.mogreet.com/moms/transaction.send?client_id=1316&token=dbd7557a6a9d09ab13fda4b5337bc9c7&campaign_id=28420&to=' . intval($phone) . '&message='. $thankYouMsg;
+    $ch = curl_init($url); 
+    $response = curl_exec($ch);
+    curl_close($ch);
 
 
 /////////////
 //Email Logic
 /////////////
 
-$subject = $number_to_name[intval($phone)] . " just uploaded a photo!";
+    $subject = $phone . " just uploaded a photo!";
 
 ////////
 // Send
 ////////
-mail('kazi.rousseau@gmail.com', $subject, $phone);
-mail('cchanningallen@gmail.com', $subject, $phone);
-mail('svenkat45@gmail.com', $subject, $phone);
-}
+    mail('kazi.rousseau@gmail.com', $subject, $phone);
+    mail('cchanningallen@gmail.com', $subject, $phone);
+    mail('svenkat45@gmail.com', $subject, $phone);
+
+// }
+
+
 ?>
